2efcf1c8dc1d00b1ea9232728b1668e74ff64791
Nour Elden Alaa <nourelden515@gmail.com>
2022-04-25 18:04:32 +0200

    View Technician Profile (Done)

---
+++ app/src/main/java/com/example/servizi/technician/model/TechnicianResponse.kt
@@ -0,0 +1,15 @@
+package com.example.servizi.technician.model
+
+import com.squareup.moshi.Json
+
+data class TechnicianResponse(
+    @Json(name = "id") val id: Int,
+    @Json(name = "firstName") val firstName: String,
+    @Json(name = "lastName") val lastName: String,
+    @Json(name = "email") val email: String,
+    @Json(name = "phone") val phone: String,
+    @Json(name = "city") val city: String,
+    @Json(name = "governorate") val governorate: String,
+    @Json(name = "profession") val profession: String,
+    @Json(name = "createdAt") val createdAt: String,
+)
